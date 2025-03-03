ef1dfcb708ecfea5f3b768eb9a39785deee6cf67
sasatee <sarvam2601@gmail.com>
2025-03-03 19:44:45 +0400

    Update SQLite database connection path in appsettings.json
    Modify the DefaultConnection string to use a relative path for the SQLite database file, simplifying the database configuration for local development and deployment

--- backend/AuthenticationAPI/Migrations/20250303153612_InitialCreate.Designer.cs
+++ backend/AuthenticationAPI/Migrations/20250303153612_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuthenticationAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20250303153228_InitialCreate")]
+    [Migration("20250303153612_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
